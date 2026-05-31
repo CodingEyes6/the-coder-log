b337e6e6a5e67e6d7b953268cfba43002d793e26
Usama Sarfraz <osama.sarfraaz@gmail.com>
2026-06-01 01:13:13 +0500

    Refactor code structure for improved readability and maintainability

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The Coder Log';
+export const SITE_DESCRIPTION = 'Android & Flutter Development Diary - Learning in Public';
